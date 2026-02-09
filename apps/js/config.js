eb3d724512ce65a1d4493b214bc2ee99600f7a06
619amandagaiser-del <619amandagaiser@gmail.com>
2026-02-08 16:09:31 -0800

    Update config.js

--- apps/js/config.js
+++ apps/js/config.js
@@ -1,16 +1,10 @@
-const SystemApps = {
-    // ... your other apps (stargate, tv, profile, ai)
-    'drive': { 
-        title: 'Google Drive', 
-        url: 'apps/drive.html', 
-        icon: 'cloud_queue', 
-        color: '#4285F4' 
-    }
+const DRIVE_CONFIG = {
+    clientId: '106130595767-2ai0d8mac2ard8q2ngthuuh5pahoutuv.apps.googleusercontent.com',
+    // Leave API Key blank if you are only using OAuth2, or add yours from Google Console
+    apiKey: 'YOUR_API_KEY_HERE', 
+    scopes: 'https://www.googleapis.com/auth/drive.file',
+    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]
 };
 
-// Google API Credentials (You'll need these from Google Cloud Console)
-const DRIVE_CONFIG = {
-    clientId: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
-    apiKey: 'YOUR_API_KEY',
-    scopes: 'https://www.googleapis.com/auth/drive.file'
-};
+// This variable will hold the session once you log in
+let DRIVE_ACCESS_TOKEN = null;
